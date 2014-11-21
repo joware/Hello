ba0c20b8a0f1cdc28d345ee7dbc7850249330379
lizhongwei <lizhongwei.hust@gmail.com>
2014-11-21 12:57:07 +0800

    add coverage part

--- travis.sh
+++ travis.sh
@@ -28,6 +28,26 @@ mkdir output
 cp Demo/bin/Demo-instrumented.apk output/Demo.apk
 cp DemoTest/bin/DemoTest-debug.apk output/DemoTest.apk
 
+	echo ======= install apk =====
+	cd output
+	adb uninstall com.oops.demo
+	adb uninstall com.oops.demo.test
+	adb install Demo.apk
+	adb install DemoTest.apk
+    cd ..
+
+
+	echo ======== Run Unit test ========
+	cd Demo
+
+    adb shell am instrument -w -e reportDir /sdcard/ut/ -e reportFile UnitTestReport.xml -e coverage true -e coverageFile /sdcard/ut/coverage.ec com.oops.demo.test/com.zutubi.android.junitreport.JUnitReportTestRunner
+
+    echo ========= get Report =========
+    adb pull /sdcard/ut/UnitTestReport.xml
+    adb pull /sdcard/ut/coverage.ec
+    java -cp ${ANDROID_HOME}/tools/lib/emma.jar emma report -r xml -in bin/coverage.em -in coverage.ec  -sp ./src/
+    java -cp ${ANDROID_HOME}/tools/lib/emma.jar emma report -r html -in bin/coverage.em -in coverage.ec  -sp ./src/
+	cd ..
 
 
 
